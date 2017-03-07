15ad0950711e523373edef3590d2b2b66ff7a230
seandong <sindon@gmail.com>
2017-03-07 18:18:43 +0800

    CHG: validate token expires_at in redis

--- lib/wework/js_ticket/redis_store.rb
+++ lib/wework/js_ticket/redis_store.rb
@@ -13,7 +13,7 @@ module Wework
       end
 
       def expired?
-        redis.hvals(key).empty?
+        redis.hvals(key).empty? || redis.hget(key, "expires_at") <= Time.now.to_i
       end
 
       def refresh_token
