631e312bc2ccc814a5b09a8c226d95bd0120672a
seandong <sindon@gmail.com>
2017-06-06 14:34:54 +0800

    FIX: authorization url

--- lib/wework/api/agent.rb
+++ lib/wework/api/agent.rb
@@ -7,7 +7,7 @@ module Wework
 
       def authorize_url(redirect_uri, scope="snsapi_base", state="wxwork")
         uri = ERB::Util.url_encode(redirect_uri)
-        "#{AUTHORIZE_ENDPOINT}?appid=#{corp_id}&redirect_uri=#{uri}&response_type=code&scope=#{scope}&state=#{state}#wechat_redirect"
+        "#{AUTHORIZE_ENDPOINT}?appid=#{corp_id}&redirect_uri=#{uri}&response_type=code&scope=#{scope}&agentid=#{app_id}&state=#{state}#wechat_redirect"
       end
 
       def get_oauth_userinfo code
