9af54fe8b8831b4fda28c3bd1bd3c4dba7f8eea8
@seandong <sindon@gmail.com>
2017-06-20 12:21:13 +0800

    FIX: test

--- test/wework/agent_test.rb
+++ test/wework/agent_test.rb
@@ -6,10 +6,6 @@ class Wework::AgentTest < Minitest::Test
 
   def setup
     @agent = Wework::Agent.new(corp_id: ENV['CORP_ID'], agent_id: ENV['APP_ID'], app_secret: ENV['APP_SECRET'])
-    # @image_media_id = '1t7ArQ9p2kkebFgIhl5AV0R0GW0Um14sF8LhF1WQ_ZyDIx_5gunSGsI-OZWMc3-cC'
-    # @file_media_id = '1gzwVHjceHuh-n6YljtcTmmENyxIBh16BVRRFH4yu9Ss'
-    # @voice_media_id = '1_ESizV0Qiw1LQAxt-LWYJK23-5u5RDKYXQQ9M5fDBV5PNoWGMGsea5A9vOVWgmGi'
-    # @video_media_id = '1KPqSJKKdQ7qmTeoK4Nhd80P7loOaiPoI29xrXuF-e4ywY-HHzIHKFHvH7b4N0gaA'
   end
 
   def test_authorize_url
@@ -27,88 +23,105 @@ class Wework::AgentTest < Minitest::Test
     assert_equal ENV['CORP_ID'], package['appId']
   end
 
-  # def test_access_token
-  #   assert agent.access_token
-  # end
+  def test_access_token
+    assert agent.access_token
+  end
 
-  # def test_get_info
-  #   puts agent.get_info
-  # end
+  def test_get_info
+    assert agent.get_agent.success?
+  end
 
-  # def test_set_info
-  #   result = agent.media_upload('image', File.join(File.dirname(__FILE__), '../../fixtures/zhiren.png'))
-  #   p result
-  #   puts agent.set_info(name: '知人v2', redirect_domain: 'zhirenhr.com', description: '智能人力资源管理专家', logo_mediaid: result.media_id)
-  # end
+  def test_set_info
+    result = agent.set_agent(
+      name: '知人',
+      redirect_domain: 'zhiren.com',
+      description: '智能人力资源管理专家',
+      logo_mediaid: image_id
+    )
 
-  # def test_menu_create
-  #   menu = {
-  #     button: [
-  #       {
-  #         type: 'view',
-  #         name: '自助服务',
-  #         url: 'https://zhiren.com'
-  #       }
-  #     ]
-  #   }
+    assert result.success?
+  end
 
-  #   puts agent.menu_create(menu)
-  # end
+  def test_menu_create
+    menu = {
+      button: [
+        {
+          type: 'view',
+          name: '自助服务',
+          url: 'https://zhirenhr.com'
+        }
+      ]
+    }
 
-  # def test_menu_delete
-  #   puts agent.menu_delete
-  # end
+    assert agent.menu_create(menu).success?
+  end
 
-  # def test_media_upload
-  #   puts agent.media_upload('image', File.join(File.dirname(__FILE__), '../../fixtures/zhiren.png'))
-  #   puts agent.media_upload('image', File.join(File.dirname(__FILE__), '../../fixtures/sample.txt'))
-  #   puts agent.media_upload('voice', File.join(File.dirname(__FILE__), '../../fixtures/sample.amr'))
-  #   puts agent.media_upload('video', File.join(File.dirname(__FILE__), '../../fixtures/sample.mp4'))
-  # end
+  def test_menu_delete
+    assert agent.menu_delete.success?
+  end
 
-  # def test_media_get
-  #   puts agent.media_get @image_media_id
-  # end
+  def test_media_get
+    assert_kind_of Tempfile, agent.media_get(image_id)
+  end
 
-  # def test_text_message_send
-  #   puts agent.text_message_send('@all', '', '这是一条来自API的测试消息')
-  # end
+  def test_text_message_send
+    assert agent.text_message_send('@all', '', '这是一条来自API的测试消息').success?
+  end
 
-  # def test_image_message_send
-  #   puts agent.image_message_send '@all', '', @image_media_id
-  # end
-  #
-  # def test_voice_message_send
-  #   puts agent.voice_message_send '@all', '', @voice_media_id
-  # end
+  def test_image_message_send
+    assert agent.image_message_send('@all', '', image_id).success?
+  end
 
-  # def test_video_message_send
-  #   puts agent.video_message_send '@all', '', {media_id: @video_media_id, title: '测试', description: '来自API的测试视频'}
-  # end
+  def test_voice_message_send
+    assert agent.voice_message_send('@all', '', voice_id).success?
+  end
 
-  # def test_file_message_send
-  #   puts agent.file_message_send '@all', '', @file_media_id
-  # end
-  #
-  # def test_textcard_message_send
-  #   text_card = {
-  #     title: "测试通知",
-  #     description: "<div class=\"gray\">2016年9月26日</div><br/><br/><br/><br/><div class=\"normal\">大字体呀</div><div class=\"highlight\">我是居中的...</div>",
-  #     url: 'https://zhiren.com',
-  #     btn: '查看详情'
-  #   }
-  #   p agent.textcard_message_send '@all', '', text_card
-  # end
+  def test_video_message_send
+    assert agent.video_message_send('@all', '', {media_id: video_id, title: '测试', description: '来自API的测试视频'}).success?
+  end
 
-  # def test_news_message_send
-  #   news = [
-  #     {
-  #       title: "中秋节礼品领取",
-  #       description: "今年中秋节公司有豪礼相送",
-  #       url: "https://zhiren.com",
-  #       picurl: "http://res.mail.qq.com/node/ww/wwopenmng/images/independent/doc/test_pic_msg1.png"
-  #      }
-  #   ]
-  #   puts agent.news_message_send '@all', '', news
-  # end
+  def test_file_message_send
+    assert agent.file_message_send('@all', '', file_id).success?
+  end
+
+  def test_textcard_message_send
+    text_card = {
+      title: "测试通知",
+      description: "<div class=\"gray\">2016年9月26日</div><br/><br/><br/><br/><div class=\"normal\">大字体呀</div><div class=\"highlight\">我是居中的...</div>",
+      url: 'https://zhiren.com',
+      btn: '查看详情'
+    }
+
+    assert agent.textcard_message_send('@all', '', text_card).success?
+  end
+
+  def test_news_message_send
+    news = [
+      {
+        title: "中秋节礼品领取",
+        description: "今年中秋节公司有豪礼相送",
+        url: "https://zhiren.com",
+        picurl: "http://res.mail.qq.com/node/ww/wwopenmng/images/independent/doc/test_pic_msg1.png"
+       }
+    ]
+
+    assert agent.news_message_send('@all', '', news).success?
+  end
+  private
+
+  def image_id
+    @image_id ||= '1_adABT-H035nwE-zgShL8mZlJQ4d29Xuvfliy3ynzZAhMW5vBoka0P2iAl2daoJU' #@agent.media_upload('image', File.join(File.dirname(__FILE__), '../fixtures/zhiren.png')).media_id
+  end
+
+  def file_id
+    @file_id ||= '1WGPDpW5r9U6E4vjmcfYJB1vrqOn7nGPuDRR4O9ql0n8' #@agent.media_upload('image', File.join(File.dirname(__FILE__), '../fixtures/sample.txt')).media_id
+  end
+
+  def voice_id
+    @voice_id ||= '1wIrsgLbIZ-Kn6oUbqnKQR7eE896F-f1J5lTwkQe0aSWhyhKN1uPFCVertUca7gG1' #@agent.media_upload('image', File.join(File.dirname(__FILE__), '../fixtures/sample.amr')).media_id
+  end
+
+  def video_id
+    @video_id ||= '17kscUIzs_0_bCuugYDv37tjLqewnTOUvj8J59cT5Qg2FTLB8iySF7a1YMCnMBkF-' #@agent.media_upload('image', File.join(File.dirname(__FILE__), '../fixtures/sample.mp4')).media_id
+  end
 end
--- test/wework/contact_test.rb
+++ test/wework/contact_test.rb
@@ -12,6 +12,33 @@ class Wework::ContactTest < Minitest::Test
     assert contact.access_token
   end
 
+  def test_user
+    userid = '1dfb4e6c58543c33b74af316acb1bec6'
+    user = {
+      userid: userid,
+      name: '老董',
+      mobile: '18683689949',
+      department: [1],
+      english_name: 'seandong',
+      position: '工程师',
+    }
+    assert contact.user_create(user).success?
+    assert contact.user_update(userid, {name: '山居中人'}).success?
+    assert contact.user_get(userid).success?
+    assert contact.user_delete(userid).success?
+    assert contact.user_simplelist(1, 1).success?
+    assert contact.user_list(1, 1).success?
+  end
+
+  def test_department
+    department_id = rand(100000 .. 999999)
+    department = {name: "API测试部门#{department_id}", id: department_id, parentid: 1}
+    assert contact.department_create(department).success?
+    assert contact.department_update(department_id, {name: "API测试部门#{rand(100000 .. 999999)}"}).success?
+    assert contact.department_list(department_id).success?
+    assert contact.department_delete(department_id).success?
+  end
+
   # def test_batch_replaceparty
   #   result = contact.media_upload('file', File.join(File.dirname(__FILE__), '../../fixtures/party.csv'))
   #   p result
@@ -27,73 +54,10 @@ class Wework::ContactTest < Minitest::Test
   #   puts contact.batch_getresult(result.jobid)
   # end
 
-  # def test_media_upload
-  #   #puts contact.media_upload('file', File.join(File.dirname(__FILE__), '../../fixtures/user.csv'))
-  #   puts contact.media_upload('file', File.join(File.dirname(__FILE__), '../../fixtures/party.csv'))
-  # end
-
   # def test_batch_syncuser
   #   token ='165ff8e819520f8ae8a0000c447c0d91'
   #   encodingaeskey = '7503e83ad9c081f91730e1f3713767b651072a077e861'
   #   media_id = '1qZiLuycGBqpv0WrRsAKwqO5JpLminD17hsTDIsu8JxE'
   #   puts contact.batch_syncuser(media_id, 'https://zhiren.com/test', token, encodingaeskey)
-  # end
-
-  # def test_user_create
-  #   info = {
-  #     userid: 'Keeperlove',
-  #     name: '吴松林',
-  #     mobile: '+16479897753',
-  #     department: [1],
-  #     english_name: 'tinyfive',
-  #     position: '工程师',
-  #   }
-  #   result = contact.user_create info
-  #   puts result
-  # end
-
-  # def test_user_delete
-  #   userids = ["seandong"]
-  #   userids.each do |userid|
-  #     result = contact.user_delete userid
-  #     p result
-  #   end
-  # end
-
-  # def test_user_update
-  #   p contact.user_update 'seandong', {mobile: '18683689944'}
-  # end
-
-  # def test_user_simplelist
-  #   result = contact.user_simplelist 1, 1
-  #   puts result
-  # end
-
-  # def test_user_list
-  #   result = contact.user_list 1, 1
-  #   p result
-
-  #   p result.userlist.map{|u| u['userid']}
-  # end
-
-  # def test_user_get
-  #   result = contact.user_get '145ab09f09ba2adf676447d90ebb0c71'
-  #   p result
-  # end
-
-  # def test_department_list
-  #   result = contact.department_list
-  #   assert result.success?
-  #   p result
-  # end
-
-  # def test_department_delete
-  #   result = contact.department_delete 1
-  #   puts result
-  # end
-
-  # def test_department_create
-  #   result = contact.department_create {name: '工程组', id: 123, parentid: 1}
-  #   assert result.success?
   # end
 end
