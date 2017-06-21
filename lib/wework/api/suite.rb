0feb8b67ce733fed2f66c16c66a871036809dfda
@seandong <sindon@gmail.com>
2017-06-21 23:16:43 +0800

    ADD: api comment

--- lib/wework/api/suite.rb
+++ lib/wework/api/suite.rb
@@ -37,6 +37,7 @@ module Wework
       end
 
       def get_approval_data start_time, end_time, next_spnum=nil
+        # https://work.weixin.qq.com/api/doc#11228
         payload = {starttime: start_time, endtime: end_time}
         payload[:next_spnum] = next_spnum unless next_spnum.nil?
         post 'corp/getapprovaldata', payload
