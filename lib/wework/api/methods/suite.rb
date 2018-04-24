440029ae001e7a4318f5f3c06747d6b6f67837e0
seandong <sindon@gmail.com>
2018-04-24 15:47:06 +0800

    CHG: implement suite API

--- lib/wework/api/methods/suite.rb
+++ lib/wework/api/methods/suite.rb
@@ -7,7 +7,7 @@ module Wework
         end
 
         def get_pre_auth_code
-          result = post 'service/get_pre_auth_code', {suite_id: suite_id}
+          result = get 'service/get_pre_auth_code'
           return result.pre_auth_code if result.success?
         end
 
@@ -16,15 +16,32 @@ module Wework
         end
 
         def get_permanent_code auth_code
-          post 'service/get_permanent_code', {suite_id: suite_id, auth_code: auth_code}
+          post 'service/get_permanent_code', {auth_code: auth_code}
         end
 
         def get_auth_info auth_corpid, permanent_code
-          post 'service/get_auth_info', {suite_id: suite_id, auth_corpid: auth_corpid, permanent_code: permanent_code}
+          post 'service/get_auth_info', {auth_corpid: auth_corpid, permanent_code: permanent_code}
         end
 
         def get_corp_token auth_corpid, permanent_code
           post 'service/get_corp_token', {suite_id: suite_id, auth_corpid: auth_corpid, permanent_code: permanent_code}
+        end
+
+        def get_admin_list auth_corpid, agentid
+          post 'service/get_admin_list', {auth_corpid: auth_corpid, agentid: agentid}
+        end
+
+        def authorize_url(redirect_uri, scope="snsapi_base", state="wxwork")
+          uri = ERB::Util.url_encode(redirect_uri)
+          "#{AUTHORIZE_ENDPOINT}?appid=#{suite_id}&redirect_uri=#{uri}&response_type=code&scope=#{scope}&state=#{state}#wechat_redirect"
+        end
+
+        def get_oauth_userinfo(code)
+          get 'service/getuserinfo3rd', params: {access_token: access_token, code: code}
+        end
+
+        def get_user_detail(user_ticket)
+          post "service/getuserdetail3rd?access_token=#{access_token}", {user_ticket: user_ticket}
         end
       end
     end
